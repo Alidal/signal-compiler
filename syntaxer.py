663d53573f5e69e734c157dfde0bf5d1109cc88c
Vlad Sydorenko <vladyslav.sydorenko@gmail.com>
2016-05-18 17:16:11 +0300

    Continue working on syntax analizer

--- syntaxer.py
+++ syntaxer.py
@@ -1,4 +1,7 @@
-from utils import Lexeme, Error, SyntaxAnalizerError
+import uuid
+import inspect
+from utils import Error, SyntaxAnalizerError
+from treelib import Node, Tree
 
 
 class SyntaxAnalyzer:
@@ -7,9 +10,15 @@ class SyntaxAnalyzer:
         self.lexemas = lexemas
         self.identifiers = identifiers
         self.constants = constants
+        self.tree = Tree()
+        self.parent = None
 
     def scan(self):
         self.cur_lexeme = self.lexemas.pop(0)
+        node_name = inspect.stack()[1][3]
+        node_id = str(uuid.uuid1())
+        self.tree.create_node(node_name, node_id, parent=self.parent)
+        self.parent = node_id
 
     def error(self, text):
         error = Error(text, "Syntax", self.cur_lexeme.row, self.cur_lexeme.column)
@@ -17,25 +26,21 @@ class SyntaxAnalyzer:
         raise SyntaxAnalizerError
 
     def analyze(self):
-        self.scan()
         try:
             self.program()
         except SyntaxAnalizerError():
             pass
 
     def program(self):
+        self.scan()
         if self.cur_lexeme == "program":
-            self.scan()
             self.procedure_identifier()
             if self.cur_lexeme == ";":
-                self.scan()
                 self.block()
         elif self.cur_lexeme == "procedure":
-            self.scan()
             self.procedure_identifier()
             self.parameters_list()
             if self.cur_lexeme == ";":
-                self.scan()
                 self.block()
         else:
             self.error("Expected PROGRAM or PROCEDURE keyword")
@@ -58,16 +63,18 @@ class SyntaxAnalyzer:
 
     def constant_declarations(self):
         if self.lexeme != "const":
-            self.scan()
-            return
+            return  # <empty>
         self.scan()
         self.constant_declarations_list()
 
     def constant_declarations_list(self):
         self.constant_declaration()
-        self.constant_declarations_list()
+        self.scan()
+        if self.lexeme.code in self.identifiers:
+            self.constant_declarations_list()
 
     def constant_declaration(self):
+        # ToDo: empty
         self.constant_identifier()
         if self.lexeme != "=":
             self.error("Expected '='")
@@ -75,3 +82,7 @@ class SyntaxAnalyzer:
         if self.lexeme != ";":
             self.error("Expected ';'")
 
+    def constant(self):
+        self.scan()
+        if self.lexeme == '-':
+            
